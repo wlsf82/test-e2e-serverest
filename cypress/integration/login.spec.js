3ca82f00c1e0ea3997fe2114bc28b5101485f90f
Walmyr Filho <limaesilva@qelp.com>
2021-09-20 14:03:10 +0200

    Better describe tests

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -14,12 +14,12 @@ describe('Login', () => {
     cy.url().should('be.equal', `${baseUrl}/admin/home`)
   })
 
-  it('Alerta é exibido ao tentar fazer login com email incorreto', () => {
+  it('Erro é exibido ao tentar fazer login com email incorreto', () => {
     cy.login('misael@qc.com.br', Cypress.env('password'))
     cy.contains(alertSelector, 'Email e/ou senha inválidos').should('be.visible')
   })
 
-  it('Alerta é exibido ao tentar fazer login com senha incorreta', () => {
+  it('Erro é exibido ao tentar fazer login com senha incorreta', () => {
     cy.login(Cypress.env('user'), 'testing')
     cy.contains(alertSelector, 'Email e/ou senha inválidos').should('be.visible')
   })
@@ -31,12 +31,12 @@ describe('Login', () => {
     cy.url().should('be.equal', `${baseUrl}/login`)
   })
 
-  it('Alerta é exibido ao tentar fazer login com email invalido', () => {
+  it('Erro é exibido ao tentar fazer login com email invalido', () => {
     cy.login('m@m', Cypress.env('password'))
     cy.contains(alertSelector, 'email deve ser um email válido').should('be.visible')
   })
 
-  it('Alerta é exibido ao tentar fazer login login sem digitar email e senha', () => {
+  it('Erro é exibido ao tentar fazer login login sem digitar email e senha', () => {
     cy.get('[data-testid=entrar]').click()
     cy.contains(alertSelector, 'email não pode ficar em branco').should('be.visible')
     cy.contains(alertSelector, 'email não pode ficar em branco').should('be.visible')
