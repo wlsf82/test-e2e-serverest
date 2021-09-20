826227543c709389704656d7f0f1cdc5ad451747
Walmyr Filho <limaesilva@qelp.com>
2021-09-20 14:07:51 +0200

    Simplify login spec

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -4,14 +4,13 @@ describe('Login', () => {
   const baseUrl = Cypress.config('baseUrl')
   const alertSelector = '.alert'
 
-  beforeEach(() => {
-    cy.visit('/')
-    cy.url().should('be.equal', `${baseUrl}/login`)
-  })
+  beforeEach(() => cy.visit('/'))
 
-  it('Login com sucesso leva à página home', () => {
+  it('Login com sucesso leva à página home e logout leva à página de login', () => {
     cy.login(Cypress.env('user'), Cypress.env('password'))
     cy.url().should('be.equal', `${baseUrl}/admin/home`)
+    cy.get('[data-testid=logout]').click()
+    cy.url().should('be.equal', `${baseUrl}/login`)
   })
 
   it('Erro é exibido ao tentar fazer login com email incorreto', () => {
@@ -22,13 +21,6 @@ describe('Login', () => {
   it('Erro é exibido ao tentar fazer login com senha incorreta', () => {
     cy.login(Cypress.env('user'), 'testing')
     cy.contains(alertSelector, 'Email e/ou senha inválidos').should('be.visible')
-  })
-
-  it('Logout com sucesso leva à página de login', () => {
-    cy.login(Cypress.env('user'), Cypress.env('password'))
-    cy.url().should('be.equal', `${baseUrl}/admin/home`)
-    cy.get('[data-testid=logout]').click()
-    cy.url().should('be.equal', `${baseUrl}/login`)
   })
 
   it('Erro é exibido ao tentar fazer login com email invalido', () => {
