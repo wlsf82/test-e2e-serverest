8e30d14960f89bbb90362e557a5e57ac63e32f0f
Walmyr Filho <limaesilva@qelp.com>
2021-09-20 16:25:44 +0200

    Refactor gui custom commands

--- cypress/support/guiCommands.js
+++ cypress/support/guiCommands.js
@@ -1,51 +1,35 @@
 /// <reference types="Cypress" />
 
-import login from '../page/login.page'
-import createUser from '../page/user.page'
 import 'cypress-file-upload'
+
+const emailSelector = '[data-testid=email]'
+const nameSelector = '[data-testid=nome]'
+const passwordSelector = '[data-testid=senha]'
 
 Cypress.Commands.add('login', (user, password) => {
   cy.createUsersViaApi()
-  cy.get(login.imputEmail).type(user)
-  cy.get(login.imputSenha).type(password)
-  cy.get(login.btnEntrar).click()
+  cy.get(emailSelector).type(user)
+  cy.get(passwordSelector).type(password)
+  cy.get('[data-testid=entrar]').click()
 })
 
-Cypress.Commands.add('createUserAdm', (name, email, password) => {
-  cy.get(createUser.imputNome).type(name)
-  cy.get(createUser.imputEmail).type(email)
-  cy.get(createUser.imputSenha).type(password)
-  cy.get(createUser.btnCadastrarAdm).click()
-  cy.get(createUser.btnCadastrar).click()
-})
+Cypress.Commands.add('createUser', user => {
+  cy.get(nameSelector).type(user.name)
+  cy.get(emailSelector).type(user.email)
+  cy.get(passwordSelector).type(user.password)
 
-Cypress.Commands.add('createUser', (name, email, password) => {
-  cy.get(createUser.imputNome).type(name)
-  cy.get(createUser.imputEmail).type(email)
-  cy.get(createUser.imputSenha).type(password)
-  cy.get(createUser.btnCadastrar).click()
-})
+  if (user.admin) {
+    cy.get('[data-testid=checkbox]').check()
+  }
 
-Cypress.Commands.add('createUserAdminAdm', (name, email, password) => {
-  cy.get(createUser.imputNome).type(name)
-  cy.get(createUser.imputEmail).type(email)
-  cy.get(createUser.imputSenha).type(password)
-  cy.get(createUser.btnCadastrarAdm).click()
-  cy.get(createUser.btnCadastrarAdmin).click()
-})
-
-Cypress.Commands.add('createAdminUser', (name, email, password) => {
-  cy.get(createUser.imputNome).type(name)
-  cy.get(createUser.imputEmail).type(email)
-  cy.get(createUser.imputSenha).type(password)
-  cy.get(createUser.btnCadastrarAdmin).click()
+  cy.contains('button', 'Cadastrar').click()
 })
 
 Cypress.Commands.add('fillProductFormAndSubmit', product => {
-  cy.get('[data-testid=nome]').type(product.nome)
+  cy.get(nameSelector).type(product.nome)
   cy.get('[data-testid=preco]').type(product.preco)
   cy.get('[data-testid=descricao]').type(product.descricao)
   cy.get('[data-testid=quantidade]').type(product.quantidade)
-  cy.get('[data-testid=imagem]').as('fileInput').attachFile('cy.png')
+  cy.get('[data-testid=imagem]').attachFile('cy.png')
   cy.get('[data-testid=cadastarProdutos]').click()
 })
