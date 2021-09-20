5541e47cf8ae2f3c7dd7616224a541c40f636127
Walmyr Filho <limaesilva@qelp.com>
2021-09-20 10:39:16 +0200

    Finish refactor of home page tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,142 +1,148 @@
 /// <reference types = "Cypress" />
 
 const baseUrl = Cypress.config('baseUrl')
+const menuHomeSelector = '[data-testid=home]'
 
-describe('Homepage as admin', () => {
+describe('Homepage - Admin', () => {
   beforeEach(() => {
     cy.bypassAdminLogin()
     cy.visit('/admin/home')
   })
 
   it('URL é "/admin/home", logo está visível e parágrapho está correto', () => {
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/home`)
-    cy.get('.lead')
-      .should('have.text', 'Este é seu sistema para administrar seu ecommerce.')
-      .and('be.visible')
+    cy.url().should('be.equal', `${baseUrl}/admin/home`)
+    cy.contains('.lead', 'Este é seu sistema para administrar seu ecommerce.')
+      .should('be.visible')
     cy.get('.imagem').should('be.visible')
   })
 
-  it('Clique no botão "Cadastrar" da seção "Cadastrar Usuários" leva à página "/admin/cadastrarusuarios"', () => {
-    cy.get('[data-testid=cadastrarUsuarios]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/cadastrarusuarios`)
+  context('Cadastrar Usuários', () => {
+    const createUsersUrl = `${baseUrl}/admin/cadastrarusuarios`
+
+    it('Clique no botão "Cadastrar" da seção "Cadastrar Usuários" leva à página "/admin/cadastrarusuarios"', () => {
+      cy.get('[data-testid=cadastrarUsuarios]').click()
+      cy.url().should('be.equal', createUsersUrl)
+    })
+
+    it('Clique no botão "Cadastrar Usuários" do menu leva à página "/admin/cadastrarusuarios"', () => {
+      cy.get('[data-testid=cadastrar-usuarios]').click()
+      cy.url().should('be.equal', createUsersUrl)
+    })
   })
 
-  it('Clique no botão "Cadastrar Usuários" do menu leva à página "/admin/cadastrarusuarios"', () => {
-    cy.get('[data-testid=cadastrar-usuarios]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/cadastrarusuarios`)
+  context('Listar Usuários', () => {
+    const listUsersUrl = `${baseUrl}/admin/listarusuarios`
+
+    it('Clique no botão "Listar" da seção "Listar Usuários" leva à página "/admin/listarusuarios"', () => {
+      cy.get('[data-testid=listarUsuarios]').click()
+      cy.url().should('be.equal', listUsersUrl)
+    })
+
+    it('Clique no botão "Listar Usuários" do menu leva à página "/admin/listarusuarios"', () => {
+      cy.get('[data-testid=listar-usuarios]').click()
+      cy.url().should('be.equal', listUsersUrl)
+    })
   })
 
-  it('Clique no botão "Listar" da seção "Listar Usuários" leva à página "/admin/listarusuarios"', () => {
-    cy.get('[data-testid=listarUsuarios]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/listarusuarios`)
+  context('Cadastrar Produtos', () => {
+    const createProductsUrl = `${baseUrl}/admin/cadastrarprodutos`
+
+    it('Clique no botão "Cadastrar" da seção "Cadastrar Produtos" leva à página "/admin/cadastrarprodutos"', () => {
+      cy.get('[data-testid=cadastrarProdutos]').click()
+      cy.url().should('be.equal', createProductsUrl)
+    })
+
+    it('Clique no botão "Cadastrar Produtos" do menu leva à página "/admin/cadastrarprodutos"', () => {
+      cy.get('[data-testid=cadastrar-produtos]').click()
+      cy.url().should('be.equal', createProductsUrl)
+    })
   })
 
-  it('Clique no botão "Listar Usuários" do menu leva à página "/admin/listarusuarios"', () => {
-    cy.get('[data-testid=listar-usuarios]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/listarusuarios`)
+  context('Listar Produtos', () => {
+    const listProductsUrl = `${baseUrl}/admin/listarprodutos`
+
+    it('Clique no botão "Listar" da seção "Listar Produtos" leva à página "/admin/listarprodutos"', () => {
+      cy.get('[data-testid=listarProdutos]',).click()
+      cy.url().should('be.equal', listProductsUrl)
+    })
+
+    it('Clique no botão "Listar Produtos" do menu" leva à página "/admin/listarprodutos"', () => {
+      cy.get('[data-testid=listar-produtos]').click()
+      cy.url().should('be.equal', listProductsUrl)
+    })
   })
 
-  it('Clique no botão "Cadastrar" da seção "Cadastrar Produtos" leva à página "/admin/cadastrarprodutos"', () => {
-    cy.get('[data-testid=cadastrarProdutos]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/cadastrarprodutos`)
-  })
+  context('Relatórios', () => {
+    const reportsUrl = `${baseUrl}/admin/relatorios`
 
-  it('Clique no botão "Cadastrar Produtos" do menu leva à página "/admin/cadastrarprodutos"', () => {
-    cy.get('[data-testid=cadastrar-produtos]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/cadastrarprodutos`)
-  })
+    it('Clique no botão "Ver" da seção "Relatórios" leva à página "/admin/relatorios"', () => {
+      cy.get('[data-testid=relatorios]').click()
+      cy.url().should('be.equal', reportsUrl)
+    })
 
-
-  it('Clique no botão "Listar" da seção "Listar Produtos" leva à página "/admin/listarprodutos"', () => {
-    cy.get('[data-testid=listarProdutos]',).click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/listarprodutos`)
-  })
-
-  it('Clique no botão "Listar Produtos" do menu" leva à página "/admin/listarprodutos"', () => {
-    cy.get('[data-testid=listar-produtos]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/listarprodutos`)
-  })
-
-  it('Clique no botão "Ver" da seção "Relatórios" leva à página "/admin/relatorios"', () => {
-    cy.get('[data-testid=relatorios]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/relatorios`)
-  })
-
-  it('Clique no botão "Relatórios" do menu leva à página "/admin/relatorios"', () => {
-    cy.get('[data-testid=link-relatorios]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/relatorios`)
+    it('Clique no botão "Relatórios" do menu leva à página "/admin/relatorios"', () => {
+      cy.get('[data-testid=link-relatorios]').click()
+      cy.url().should('be.equal', reportsUrl)
+    })
   })
 
   it('Estando em outra página, clique no menu "Home" leva à página home', () => {
     cy.get('[data-testid=link-relatorios]').click()
-    cy.get('[data-testid=home]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/admin/home`)
+    cy.get(menuHomeSelector).click()
+    cy.url().should('be.equal', `${baseUrl}/admin/home`)
   })
 
   it('Faz logout com sucesso', () => {
     cy.get('[data-testid=logout]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/login`)
+    cy.url().should('be.equal', `${baseUrl}/login`)
   })
 })
 
-describe('Home - User', () => {
+describe('Homepage - User', () => {
+  const cartSelector = '[data-testid=carrinho]'
+  const homeUrl = `${baseUrl}/home`
+
   beforeEach(() => {
     cy.bypassUserLogin()
-    cy.visit('home')
+    cy.visit('/home')
   })
 
-  it('valida home user', () => {
-    cy.url()
-      .should('be.equal', `${baseUrl}/home`)
-    cy.get('#navbarTogglerDemo01 > .imagem').should('be.visible')
+  it('URL é "/home" e logo está visível', () => {
+    cy.url().should('be.equal', homeUrl)
+    cy.get('#navbarTogglerDemo01 .imagem').should('be.visible')
   })
 
-  it('Listar carrinho', () => {
-    cy.get('[data-testid=carrinho]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/carrinho`)
+  it('Clique no menu "Carrinho" leva à página "/carrinho"', () => {
+    cy.get(cartSelector).click()
+    cy.url().should('be.equal', `${baseUrl}/carrinho`)
   })
 
-  it('Home', () => {
-    cy.get('[data-testid=carrinho]').should('be.visible')
-    cy.get('[data-testid=home]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/home`)
+  it('Estando em outra página, clique no menu "Home" leva à página home', () => {
+    cy.get(cartSelector).click()
+    cy.get(menuHomeSelector).click()
+    cy.url().should('be.equal', homeUrl)
   })
 
-  it('Lista de compra', () => {
+  it('Clique no menu "Lista de Compras" leva à página "/minhaListaDeProdutos"', () => {
     cy.get('[data-testid=lista-de-compras]').click()
-    cy.url()
-      .should('be.equal', `${baseUrl}/minhaListaDeProdutos`)
+    cy.url().should('be.equal', `${baseUrl}/minhaListaDeProdutos`)
   })
 
   context('Produtos', () => {
-    beforeEach(() => cy.createProductViaApi())
+    it('Encontra produto ao pesquisar por produtos existente', () => {
+      const product = 'Teste Zael Uai'
 
-    it('Pesquisando produtos', () => {
-      cy.get('[data-testid=pesquisar]').type('Teste Zael Uai')
+      cy.createProductViaApi()
+
+      cy.get('[data-testid=pesquisar]').type(product)
       cy.get('[data-testid=botaoPesquisar]').click()
-      cy.get('.card').should('be.visible')
+      cy.contains('.card', product).should('be.visible')
     })
 
-    it('Pesquisando produto inexistente', () => {
+    it('Não encontra produto ao pesquisar por produto não existente', () => {
       cy.get('[data-testid=pesquisar]').type('kkkkkkkkkkkkkkk')
       cy.get('[data-testid=botaoPesquisar]').click()
-      cy.get('.espacamento > .col-12 > .row')
-        .should('have.text', 'Nenhum produto foi encontrado')
+      cy.contains('p', 'Nenhum produto foi encontrado').should('be.visible')
     })
   })
 })
